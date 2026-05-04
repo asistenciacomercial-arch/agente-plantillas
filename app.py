02f79ff8b2c45a0c3ea289a856dfd7145eb39601
asistenciacomercial-arch <asistenciacomercial@zehirut.com>
2026-05-04 14:10:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def home():
 return {"status": "ok"}
+
+@app.post("/procesar/")
+async def procesar():
+try:
+from docxtpl import DocxTemplate
+from docx import Document
+return {"mensaje": "imports OK"}
+except Exception as e:
+return {"error": str(e)}
